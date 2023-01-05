161776b9676ae5688c750de3e09a9e84f6609077
shittycodesource <razbydu@gmail.com>
2023-01-05 22:21:42 +1000

    add date separator

--- src/components/Tasks/TasksList.vue
+++ src/components/Tasks/TasksList.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="tasks__list">
         <template v-if="list.length">
-            <Task
-                v-for="task_data in list"
-                :text="task_data.text"
-                :id="task_data.id"
-                :key="task_data.id"
-            />
+
+            <template  v-for="task_data, index in list" >
+
+                 <template v-if="index == 0 || task_data.dayValue != list[index - 1].dayValue">
+                    <div class="tasks__separator" :key="index">{{ task_data.dayValue }}</div>
+                </template>
+
+                <Task
+                    :text="task_data.text"
+                    :id="task_data.id"
+                    :key="task_data.id"
+                />
+
+            </template>
+            
         </template>
         <template v-else>
             <p class="center">Empty</p>
@@ -27,7 +36,7 @@ import Task from './Task.vue';
         },
         components: {
             Task
-        }
+        },
     }
 </script>
 <style lang="scss">
@@ -37,6 +46,11 @@ import Task from './Task.vue';
             align-items: center;
             flex-wrap: wrap;
             gap: 20px;
+        }
+
+        &__separator {
+            width: 100%;
+            color: #7a7a7a;
         }
     }
 </style>
--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,15 +1,57 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { format } from 'date-fns'; 
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        tasks: []   
+        tasks: [
+            {
+                text: 'first task',
+                dayValue: '3 March 2007',
+                date: 1172810970320,
+                id: 1172810970320
+            },
+            {
+                text: 'before first task',
+                dayValue: '7 July 2005',
+                date: 1122790970320,
+                id: 1122790970320
+            },
+            {
+                text: 'middle task',
+                dayValue: '12 December 2022',
+                date: 1672310952752,
+                id: 1672310952752,
+            },
+            { 
+                text: 'lastest task',
+                dayValue: '1 January 2023',
+                date: 1672810784122,
+                id: 1672810784122,
+            },
+            {
+                text: 'before last task',
+                dayValue: '1 January 2023',
+                date: 1672610961888,
+                id: 1672610961888,
+            },
+        ]   
     },
     mutations: {
         ADD_TASK(state, data) {
-            state.tasks.unshift({text: data, id: Date.now()});
+            const date_now = Date.now();
+
+            const dayValue = format(new Date(), 'd MMMM yyyy');
+            console.log(dayValue);
+
+            state.tasks.push({
+                text: data, 
+                date: date_now,
+                id: date_now, 
+                dayValue: dayValue
+            });
         },
         DELETE_TASK(state, id) {
             state.tasks = state.tasks.filter(item => item.id != id);
@@ -26,6 +68,10 @@ export default new Vuex.Store({
         }
     },
     getters: {
-        getTasks: state => state.tasks 
+        getTasks(state) {
+            const arr = state.tasks.sort((a, b) => b.date - a.date);
+
+            return arr;
+        }
     }
 }); 
